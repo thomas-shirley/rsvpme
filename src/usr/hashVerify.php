57922dc35de2c3ba8225f9ef0ac0f113dc903e05
Thomas Shirley <5425280+thomas-shirley@users.noreply.github.com>
2023-05-23 15:28:28 +0100

    Removed legacy notes

--- src/usr/hashVerify.php
+++ src/usr/hashVerify.php
@@ -3,8 +3,6 @@
 /**
  * This class either creates a new PIN for validating mail sends or validates pins themselves.
  * @param DBConnection a PostgreSQL DB Connection passed into the constructor.
- * @param pin needs a sso that already exists within NBCUs corporate network
- * @return bool whether or not the pin & e-mail address are validated.
  * @author Thomas Shirley - hello@thomas.shirley.com
  * @version 0.1
  */
